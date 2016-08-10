aece1d9427e3d3caa8b6352714bbb809b0b5cbad
Fahsl, Eric M <eric.m.fahsl@accenture.com>
2016-08-10 14:26:33 -0700

    updaed packages, added writing to ElasticSearch instance

--- server/src/twitter-stream.ts
+++ server/src/twitter-stream.ts
@@ -4,44 +4,63 @@ let Twitter = require('twitter');
 let sentiment = require('sentiment');
 let swearJar = require('swearjar');
 let API = require('./api-management.json');
+let elasticsearch = require('elasticsearch');
 
-export class TwitterStream {
-  
-  public client: any;
-  private emitter:any;
-  private streamingTerm: string;
-  
-  constructor(_emitter:any) {
-    this.streamingTerm = API.streaming_term;
-    this.emitter = _emitter;
-    this.client = new Twitter(API);
-    this.stream();
-  }
-  
-  private stream() {
-    this.client.stream('statuses/filter', {track: this.streamingTerm}, (stream) => {
-      stream.on('data', (tweet) => {
-        //console.log([tweet.text, new Date()]);
-        tweet['sentiment'] = sentiment(tweet.text);
-        tweet['created_at'] = new Date();
-        tweet.topic = this.cleanTopic(this.streamingTerm);
-        if (swearJar.profane(tweet.text)) {
-          tweet.text = swearJar.censor(tweet.text);
-        }
-        this.emitter.notifyTweet(tweet);
-        //epoch_millis | getTime()
-      });
-      
-      stream.on('error', function (error) {
-        console.log(error);
-      });
-    });
-  }
-  
-  private cleanTopic(topic) {
-    topic = topic.split(',')[0];
-    topic = topic.charAt(0).toUpperCase() + topic.slice(1);
-    return topic;
-  }
-  
+let esClient = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'error'
+});
+
+function loadIntoEs(obj) {
+  esClient.create({
+    index: 'twitter3',
+    type: 'tweet',
+    body: obj
+  }, function (error, response) {
+    if (error) {
+      console.log('elasticsearch error: ' + error);
+    }
+  });
 }
+
+  export class TwitterStream {
+
+    public client: any;
+    private emitter: any;
+    private streamingTerm: string;
+
+    constructor(_emitter: any) {
+      this.streamingTerm = API.streaming_term;
+      this.emitter = _emitter;
+      this.client = new Twitter(API);
+      this.stream();
+    }
+
+    private stream() {
+      this.client.stream('statuses/filter', { track: this.streamingTerm }, (stream) => {
+        stream.on('data', (tweet) => {
+          //console.log([tweet.text, new Date()]);
+          tweet['sentiment'] = sentiment(tweet.text);
+          tweet['created_at'] = new Date();
+          tweet.topic = this.cleanTopic(this.streamingTerm);
+          if (swearJar.profane(tweet.text)) {
+            tweet.text = swearJar.censor(tweet.text);
+          }
+          this.emitter.notifyTweet(tweet);
+          loadIntoEs(tweet);
+          //epoch_millis | getTime()
+        });
+
+        stream.on('error', function (error) {
+          console.log(error);
+        });
+      });
+    }
+
+    private cleanTopic(topic) {
+      topic = topic.split(',')[0];
+      topic = topic.charAt(0).toUpperCase() + topic.slice(1);
+      return topic;
+    }
+
+  }
